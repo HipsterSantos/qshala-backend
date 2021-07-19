4850dd7f1075d76a24bdffb890a923df5ccb4b86
HipsterSantos <santoscampos269@gmail.com>
2021-07-19 22:33:07 +0100

    setting up multer and view engine

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express'); 
 const app = express() ; 
-const 
-const PORT  = process.env.PORT || 3000;
+const multer = require('multer');
 
+app.use(express.static(__dirname + '/public')) ;
+app.use(express.json());
+app.use(express.urlencoded());
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/uploads/')},
+        filename: function (req, file, cb) {
+            cb(null, file.originalname+Date.now());
+        }
+    });
+
+const PORT  = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
     console.log(`'listening on port ' ${PORT}`);
 });
